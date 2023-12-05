ac5d3b5d734655ae59ac018925b4cbd89e45d005
RavenelMaelys <maelys.ravenel@viacesi.fr>
2023-12-05 13:47:07 +0100

    Pickup Animals

---
+++ Assets/Scripts/Pickup.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+
+public class Pickup : MonoBehaviour
+{
+    public InputActionReference pickup;
+
+    public TMP_Text animalSaved;
+
+    private int i = 1;
+
+    private void Update()
+    {
+        if (pickup.action.triggered)
+        {
+            Pick();
+        }
+    }
+
+    public void Pick()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider.tag == "Animal")
+            {
+                hit.collider.gameObject.SetActive(false);
+                animalSaved.text = "Saved : " + i;
+                i++;
+            }
+        }
+    }
+
+
+}
