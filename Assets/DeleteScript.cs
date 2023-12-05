1028dfbe8262a2f138b8316b0ff5d2a7bc017854
SwiFFeR <tanguy.drieu@viacesi.fr>
2023-12-05 14:10:17 +0100

    added sounds and build

--- Assets/DeleteScript.cs
+++ Assets/DeleteScript.cs
@@ -6,7 +6,7 @@ public class DeleteScript : MonoBehaviour
 {
     public InputActionReference deleteRef;
     public playerHealth pH;
-    //private AudioManager audioManager;
+    public AudioSource deletionSound;
 
     private void Start()
     {
@@ -29,17 +29,25 @@ public class DeleteScript : MonoBehaviour
             if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Eatable"))
             {
                 Destroy(hit.collider.gameObject);
-
+                PlayDeletionSound();
                 pH.onEat("Eatable");
 
             }
             else if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Trash"))
             {
                 Destroy(hit.collider.gameObject);
-
+                PlayDeletionSound();
                 pH.onEat("Trash");
 
             }
+        }
+    }
+
+   void PlayDeletionSound()
+    {
+        if (deletionSound != null)
+        {
+            deletionSound.Play();
         }
     }
 
