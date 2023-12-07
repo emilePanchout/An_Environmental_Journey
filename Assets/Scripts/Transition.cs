12b4dd11db8e74617a1fb3af497f42ad2857afe9
Héléna <helena.oursel@gmail.com>
2023-12-07 08:34:43 +0100

    Change

---
+++ Assets/Scripts/Transition.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transition : MonoBehaviour
+{
+    public static string CrossTitle;
+    public static string CrossParagraphe;
+}
