1e5b6982670241cba4c8c2e4e6467c30e6287541
RavenelMaelys <maelys.ravenel@viacesi.fr>
2023-12-08 16:33:04 +0100

    Spotlight PC

--- Assets/Scripts/Forest/PCManager.cs
+++ Assets/Scripts/Forest/PCManager.cs
@@ -6,6 +6,14 @@ using UnityEngine.InputSystem;
 public class PCManager : MonoBehaviour
 {
     public Camera xr_camera;
+    public Camera pc;
+    public GameObject Spot;
+
+    private void Start()
+    {
+        Spot.SetActive(false);
+    }
+
     public void ChangeCamera(InputAction.CallbackContext context)
     {
         if (xr_camera.depth == -1)
@@ -16,5 +24,23 @@ public class PCManager : MonoBehaviour
         {
             xr_camera.depth = -1;
         }
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        Ray ray = pc.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            Debug.Log(hit.collider.gameObject.name);
+            if (hit.collider.gameObject.name == "Point")
+            {
+                Debug.Log(hit.collider.gameObject.name);
+                Spot.SetActive(true);
+                Spot.transform.parent = hit.collider.gameObject.transform.parent.transform;
+                Spot.transform.position = new Vector3(hit.collider.gameObject.transform.parent.transform.position.x, 4, hit.collider.gameObject.transform.parent.transform.position.z);
+            }
+        }
+
     }
 }
